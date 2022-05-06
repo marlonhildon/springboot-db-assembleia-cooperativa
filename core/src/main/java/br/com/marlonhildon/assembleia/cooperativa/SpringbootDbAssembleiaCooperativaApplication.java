1c13b6a05d439abc13850b67e9d6e403e8d9c663
Marlon Hildon <marlonhildon@gmail.com>
2022-05-05 22:42:35 -0300

    Estrutura base do projeto springboot

---
+++ core/src/main/java/br/com/marlonhildon/assembleia/cooperativa/SpringbootDbAssembleiaCooperativaApplication.java
@@ -0,0 +1,13 @@
+package br.com.marlonhildon.assembleia.cooperativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDbAssembleiaCooperativaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDbAssembleiaCooperativaApplication.class, args);
+	}
+
+}
---
+++ core/src/test/java/br/com/marlonhildon/assembleia/cooperativa/SpringbootDbAssembleiaCooperativaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marlonhildon.assembleia.cooperativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDbAssembleiaCooperativaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
